42e4f41ea570dd896cfdaafc9253d26fe7f29975
Waseem-Isaac <waseemisac@gmail.com>
2023-05-24 15:19:57 +0300

    fix: IslamicI18n WEEKDAYS order: The weekdays order didn't follow the one provided by Ngb-datepicker at: https://ng-bootstrap.github.io/#/components/datepicker/calendars#islamicumalqura

--- projects/ngx-hijri-gregorian-datepicker/src/lib/hijri-gregorian-datepicker/IslamicI18n.ts
+++ projects/ngx-hijri-gregorian-datepicker/src/lib/hijri-gregorian-datepicker/IslamicI18n.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { NgbDatepickerI18n, NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
 
-const WEEKDAYS = ['أحد', 'إثنين', 'ثلاثاء', 'أربعاء', 'خميس', 'جمعة', 'سبت'];
+const WEEKDAYS = ['إثنين', 'ثلاثاء', 'أربعاء', 'خميس', 'جمعة', 'سبت', 'أحد'];
 const MONTHS = ['محرم', 'صفر', 'ربيع الأول', 'ربيع الآخر', 'جمادى الأولى', 'جمادى الآخرة', 'رجب', 'شعبان', 'رمضان', 'شوال',
   'ذو القعدة', 'ذو الحجة'];
 
